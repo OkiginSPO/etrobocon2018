4075829f1c484ecafb62c1ffa635abcd74726f9f
izumi20180215 <s_izumi@ospo.co.jp>
2018-09-26 11:06:46 +0900

    ターン時に左右逆転して回転していたため、修正

--- src/Right/Grid.cpp
+++ src/Right/Grid.cpp
@@ -1,11 +1,11 @@
 #include "Grid.h"
 #include "math.h"
 
-static float GRID_SIZE = 200.0; //450.0; //座標のマス幅（100mm）
+//static float GRID_SIZE = 150.0; //450.0; //座標のマス幅（100mm）
 
 // 角度の算出に利用(両方200のときは正方形と仮定してのこと。本来は長方形であるべき)
-static float GRID_X = 200; // 開発部の床が50cm  225.0; // 横(45cm) その中間点をとって22.5
-static float GRID_Y = 200; // 開発部の床が50cm   200.0; // 縦(40cm) その中間点をとって20.0
+static float GRID_X = 225.0; // 開発部の床が50cm  225.0; // 横(45cm) その中間点をとって22.5
+static float GRID_Y = 200.0; // 開発部の床が50cm   200.0; // 縦(40cm) その中間点をとって20.0
 
 static float grid_distance = 0.0; //現在座標から目標座標までの距離
 static float grid_direction = 0.0; //現在座標から目標座標の方位
@@ -19,9 +19,8 @@ void Grid::init() {
 /* 座標aから座標bまでの移動距離を設定する関数 */
 void Grid::setDistance(int aX, int aY, int bX, int bY) {
     //       長方形に対応
-    //      grid_distance = sqrt( pow((float)(bX-aX) * GRID_X ,2) + pow((float)(bY-aY) * GRID_Y,2) );
-//    grid_distance = sqrt(pow((float) (bX - aX), 2) + pow((float) (bY - aY), 2)) * GRID_SIZE;
-    grid_distance = sqrt(pow((float) (bY - aY), 2) + pow((float) (bX - aX), 2)) * GRID_SIZE;
+    grid_distance = sqrt(pow((float)(bY - aY) * GRID_Y ,2) + pow((float)(bX - aX) * GRID_X,2));
+//    grid_distance = sqrt(pow((float) (bY - aY), 2) + pow((float) (bX - aX), 2)) * GRID_SIZE;
 }
 
 /* 座標aから座標bまでの移動距離を取得する関数 */
@@ -31,26 +30,17 @@ float Grid::getDistance() {
 
 /* 目標座標の方位を設定する関数 */
 void Grid::setDirection(int aX, int aY, int bX, int bY) {
-    //    float targetDir = 0.0; // 目標方位
-    //
-    //    //　座標aから座標bへの方位（ラジアン）を取得
-    //    //    targetDir = atan2((float)(bY-aY) * GRID_Y, (float)(bX-aX) * GRID_X);
-    //    targetDir = atan2((float) (bY - aY), (float) (bX - aX));
-    //    //ラジアンから度に変換
-    //    targetDir = targetDir * 180.0 / 3.14159265359;
-
-    grid_direction = CalcDirection(bX, bY, aX, aY);
-    //    grid_direction = ;
+    grid_direction = CalcDirection(aX, aY, bX, bY);
 }
 
 float Grid::CalcDirection(int aX, int aY, int bX, int bY) {
-    float targetDir = 0.0; // 目標方位
+    float targetDir = 0.0;// 目標方位
 
     //　座標aから座標bへの方位（ラジアン）を取得
-    targetDir = atan2((float) (bX - aX), (float) (bY - aY));
-
+    targetDir = atan2((float)(bY-aY) * GRID_Y, (float)(bX-aX) * GRID_X);
+//    targetDir = atan2((float)(bY-aY), (float)(bX-aX));
     //ラジアンから度に変換
-    targetDir = targetDir * 180.0 / 3.14159265359;
+    targetDir = targetDir * 180.0 /  3.14159265359;
 
     return targetDir;
 }
